b14e7ae3ddc0ca241955941e9f0aeee68dcfc8a2
ngpadovan <ngpadovan@gmail.com>
2023-08-04 18:36:00 -0700

    begin html/css for table(cards and chips)

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+
+// Build an 'original' deck of 'card' objects used to create shuffled decks
+const originalDeck = buildOriginalDeck();
+
+
+/*----- app's state (variables) -----*/
+let shuffledDeck;
+
+/*----- cached element references -----*/
+const shuffledContainer = document.getElementById('shuffled-deck-container');
+
+/*----- event listeners -----*/
+document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
+
+/*----- functions -----*/
+function getNewShuffledDeck() {
+  // Create a copy of the originalDeck (leave originalDeck untouched!)
+  const tempDeck = [...originalDeck];
+  const newShuffledDeck = [];
+  while (tempDeck.length) {
+    // Get a random index for a card still in the tempDeck
+    const rndIdx = Math.floor(Math.random() * tempDeck.length);
+    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+  }
+  return newShuffledDeck;
+}
+
+
+function buildOriginalDeck() {
+    const deck = [];
+    // Use nested forEach to generate card objects
+    suits.forEach(function(suit) {
+      ranks.forEach(function(rank) {
+          let value;
+          if (rank === 'A') {
+            value = 14;
+          } else if (rank === 'K') {
+            value = 13;
+          } else if (rank === 'Q') {
+            value = 12;
+          } else if (rank === 'J') {
+            value = 11;
+          } else {
+            value = parseInt(rank);
+          }
+    
+          deck.push({
+            face: `${suit}${rank}`,
+            value: value
+          });
+        });
+      });
+    
+      return deck;
+    }
