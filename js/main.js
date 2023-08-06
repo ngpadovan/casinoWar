61a903e04d1068a1b09285b9a70321ffcde707a7
ngpadovan <ngpadovan@gmail.com>
2023-08-05 20:35:09 -0700

    add function to initialize game and build deck

--- js/main.js
+++ js/main.js
@@ -1,44 +1,30 @@
 /*----- constants -----*/
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-
-// Build an 'original' deck of 'card' objects used to create shuffled decks
 const originalDeck = buildOriginalDeck();
 
 
 /*----- app's state (variables) -----*/
-let table, shuffledDeck, betAmt, balance
+let playercard, dealercard, bet, balance, result
 
 /*----- cached element references -----*/
-const shuffledContainer = document.getElementById('shuffled-deck-container');
+
 
 /*----- event listeners -----*/
-document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
+
 
 /*----- functions -----*/
 init()
 
 function init() {
-    render
+    let balance = 0;
+    let bet = 0;
+    let playerCard = null;
+    let dealerCard = null;
 }
-
-function getNewShuffledDeck() {
-  // Create a copy of the originalDeck (leave originalDeck untouched!)
-  const tempDeck = [...originalDeck];
-  const newShuffledDeck = [];
-  while (tempDeck.length) {
-    // Get a random index for a card still in the tempDeck
-    const rndIdx = Math.floor(Math.random() * tempDeck.length);
-    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-  }
-  return newShuffledDeck;
-}
-
 
 function buildOriginalDeck() {
     const deck = [];
-    // Use nested forEach to generate card objects
     suits.forEach(function(suit) {
       ranks.forEach(function(rank) {
           let value;
